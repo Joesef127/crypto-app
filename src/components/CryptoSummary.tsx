f3f79f5b10c72fd8c0537f50b2a282deaa2534b7
Adedayo Adegbola <adegboladayor@gmail.com>
2024-12-27 22:44:47 +0100

    refactor: did some refactoring in the portfolio.tsx

--- src/components/CryptoSummary.tsx
+++ src/components/CryptoSummary.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Crypto } from '../Types';
 
 export type CryptoPropType = {
@@ -5,12 +6,37 @@ export type CryptoPropType = {
 };
 
 export default function CryptoSummary({ crypto }: CryptoPropType): JSX.Element {
+  const [amount, setAmount] = useState<string>('0');
+
+  useEffect(() => {
+    console.log(
+      crypto.name,
+      amount,
+      parseFloat(crypto.priceUsd) * parseFloat(amount)
+    );
+  });
+
   return (
     <div>
-      <p>Rank: {crypto.rank}</p>
-      <p>Symbol: {crypto.symbol}</p>
-      <p>Name: {crypto.name}</p>
-      <p>Price (USD): ${crypto.priceUsd}</p>
+      <p>
+        Name: {crypto.name} ({crypto.symbol})
+      </p>
+      <p>Price (USD): ${parseFloat(crypto.priceUsd).toFixed(2)}</p>
+      <input
+        type="number"
+        placeholder="Enter amount"
+        value={amount}
+        onChange={(e) => {
+          setAmount(e.target.value);
+        }}
+      />
+      <p>
+        $
+        {(parseFloat(crypto.priceUsd) * parseFloat(amount)).toLocaleString(
+          undefined,
+          { minimumFractionDigits: 2, maximumFractionDigits: 2 }
+        )}
+      </p>
     </div>
   );
 }
--- src/pages/PortfolioCalc.tsx
+++ src/pages/PortfolioCalc.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import CryptoSummary from '../components/CryptoSummary';
 import { Crypto } from '../Types';
-import { Line } from 'react-chartjs-2';
-import moment from 'moment';
+// import { Line } from 'react-chartjs-2';
+// import moment from 'moment';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -13,8 +13,8 @@ import {
   Title,
   Tooltip,
   Legend,
-  ChartData,
-  ChartOptions,
+  // ChartData,
+  // ChartOptions,
 } from 'chart.js';
 
 ChartJS.register(
@@ -29,86 +29,23 @@ ChartJS.register(
 
 export default function PortfolioCalc() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
-  const [selected, setSelected] = useState<Crypto | null>();
-  const [range, setRange] = useState<string>('all');
-  const [data, setData] = useState<ChartData<'line'>>();
-  const [options, setOptions] = useState<ChartOptions<'line'>>({
-    responsive: true,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'Crypto Prices Over Time',
-      },
-    },
-  });
+  const [selected, setSelected] = useState<Crypto[]>([]);
 
-  const getInterval = (range: string) => {
-    if (range === '3') return 'h1';
-    return 'd1';
-  };
+  // const [range, setRange] = useState<string>('all');
 
-  useEffect(() => {
-    if (selected) {
-      const interval = getInterval(range);
-      const start =
-        range !== 'all'
-          ? Date.now() - parseInt(range) * 24 * 60 * 60 * 1000
-          : undefined;
-      const end = Date.now();
-
-      let url = `https://api.coincap.io/v2/assets/${selected.id}/history?interval=${interval}`;
-      if (range !== 'all') {
-        url += `&start=${start}&end=${end}`;
-      }
-
-      axios.get(url).then((response) => {
-        const responseData = response.data.data;
-
-        setData({
-          labels: responseData.map((coin: { time: string }) => {
-            return moment
-              .unix(parseInt(coin.time) / 1000)
-              .format(interval === 'h1' ? 'HH:mm' : 'DD-MM-YY');
-          }),
-          datasets: [
-            {
-              label: 'Price (USD)',
-              data: responseData.map((coin: { priceUsd: string }) =>
-                parseFloat(coin.priceUsd).toFixed(2)
-              ),
-              backgroundColor: 'rgba(75, 192, 192, 0.2)',
-              borderColor: 'rgba(75, 192, 192, 1)',
-              borderWidth: 2,
-            },
-          ],
-        });
-        setOptions({
-          responsive: true,
-          plugins: {
-            legend: {
-              display: false,
-            },
-            title: {
-              display: true,
-              text:
-                selected?.name +
-                ` Prices ` +
-                (range === 'all' ? 'Of ' : 'Over ') +
-                ((range === '3' ? '1' : undefined) ||
-                  (range === '31' ? '30' : undefined) ||
-                  (range === '8' ? '7' : undefined) ||
-                  (range === 'all' ? 'All' : undefined)) +
-                ((range === '3' ? ` Day` : undefined) ||
-                  (range === 'all' ? ` Time` : ` Days`)),
-            },
-          },
-        });
-      });
-    }
-  }, [selected, range]);
+  // const [data, setData] = useState<ChartData<'line'>>();
+  // const [options, setOptions] = useState<ChartOptions<'line'>>({
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       display: false,
+  //     },
+  //     title: {
+  //       display: true,
+  //       text: 'Crypto Prices Over Time',
+  //     },
+  //   },
+  // });
 
   useEffect(() => {
     const url = 'https://api.coincap.io/v2/assets';
@@ -116,6 +53,71 @@ export default function PortfolioCalc() {
       setCryptos(response.data.data);
     });
   }, []);
+
+  // const getInterval = (range: string) => {
+  //   if (range === '3') return 'h1';
+  //   return 'd1';
+  // };
+
+  // useEffect(() => {
+  //   if (selected) {
+  //     const interval = getInterval(range);
+  //     const start =
+  //       range !== 'all'
+  //         ? Date.now() - parseInt(range) * 24 * 60 * 60 * 1000
+  //         : undefined;
+  //     const end = Date.now();
+
+  //     let url = `https://api.coincap.io/v2/assets/${selected.id}/history?interval=${interval}`;
+  //     if (range !== 'all') {
+  //       url += `&start=${start}&end=${end}`;
+  //     }
+
+  //     axios.get(url).then((response) => {
+  //       const responseData = response.data.data;
+
+  //       setData({
+  //         labels: responseData.map((coin: { time: string }) => {
+  //           return moment
+  //             .unix(parseInt(coin.time) / 1000)
+  //             .format(interval === 'h1' ? 'HH:mm' : 'DD-MM-YY');
+  //         }),
+  //         datasets: [
+  //           {
+  //             label: 'Price (USD)',
+  //             data: responseData.map((coin: { priceUsd: string }) =>
+  //               parseFloat(coin.priceUsd).toFixed(2)
+  //             ),
+  //             backgroundColor: 'rgba(75, 192, 192, 0.2)',
+  //             borderColor: 'rgba(75, 192, 192, 1)',
+  //             borderWidth: 2,
+  //           },
+  //         ],
+  //       });
+  //       setOptions({
+  //         responsive: true,
+  //         plugins: {
+  //           legend: {
+  //             display: false,
+  //           },
+  //           title: {
+  //             display: true,
+  //             text:
+  //               selected?.name +
+  //               ` Prices ` +
+  //               (range === 'all' ? 'Of ' : 'Over ') +
+  //               ((range === '3' ? '1' : undefined) ||
+  //                 (range === '31' ? '30' : undefined) ||
+  //                 (range === '8' ? '7' : undefined) ||
+  //                 (range === 'all' ? 'All' : undefined)) +
+  //               ((range === '3' ? ` Day` : undefined) ||
+  //                 (range === 'all' ? ` Time` : ` Days`)),
+  //           },
+  //         },
+  //       });
+  //     });
+  //   }
+  // }, [selected, range]);
 
   return (
     <>
@@ -125,8 +127,8 @@ export default function PortfolioCalc() {
           name="cryptos"
           id="cryptos"
           onChange={(e) => {
-            const c = cryptos?.find((x) => x.id === e.target.value);
-            setSelected(c);
+            const c = cryptos?.find((x) => x.id === e.target.value) as Crypto;
+            setSelected([...selected, c]);
           }}
           defaultValue="bitcoin"
         >
@@ -138,26 +140,12 @@ export default function PortfolioCalc() {
               </option>
             ))}
         </select>
-        <select
-          name="days"
-          id="days"
-          onChange={(e) => {
-            setRange(e.target.value);
-          }}
-          defaultValue="all"
-        >
-          <option value="all">All Time</option>
-          <option value="31">Last 30 Days</option>
-          <option value="8">Last 7 Days</option>
-          <option value="3">Last 1 Day</option>
-        </select>
       </div>
-      {selected && <CryptoSummary crypto={selected} />}
-      {data ? (
-        <div style={{ width: 900 }}>
-          <Line options={options} data={data} />
-        </div>
-      ) : null}
+
+            {selected.map((s) => {return <CryptoSummary crypto={s} />})}
+
+      {/* {selected && <CryptoSummary crypto={selected} />} */}
+      {/* {data ? ed554r */}
     </>
   );
 }
